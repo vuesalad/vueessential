49960ec994357b46593d4c500ed6cda5d4dc0bd3
saladworks <saladworks6725@@gmail.com>
2021-02-22 10:29:22 +0800

    admin template init with vue cli and elint airbnb rule recommended

---
+++ vue2-ts-admin-template/frontapp/src/views/dashboard/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">
+      name:{{ name }}
+    </div>
+    <div class="dashboard-text">
+      roles:<span
+        v-for="role in roles"
+        :key="role"
+      >{{ role }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { UserModule } from '@/store/modules/user';
+
+@Component({
+  name: 'Dashboard',
+})
+export default class extends Vue {
+  private myName = UserModule.name;
+
+  private myRoles = UserModule.roles;
+
+  get name() {
+    return this.myName;
+  }
+
+  get roles() {
+    return this.myRoles;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
